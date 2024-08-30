45793dae8f2c9f509aedf93754056961ba77136d
Görkem Efe Dersin <dersingrkmefe2003@gmail.com>
2024-08-30 14:28:45 +0300

    Angular Pipes
    Angular Pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  name = '';
+  date = '';
+  money:number = 0;
+
+  onNameChange(value: string)
+  {
+    this.name =value;
+  }
+  onDateChange(value:string)
+  {
+    this.date = value ;
+  }
+  onMoneyChange(value:string){
+    this.money = parseFloat(value);
+  }
+}
